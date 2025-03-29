fb0413c7e1ba6328e1571680760f5564b529931d
junilhwang <junil.h@kakao.com>
2025-03-29 10:12:45 +0900

    chore: Footer에 연도 수정

--- src/components/templates/Footer.jsx
+++ src/components/templates/Footer.jsx
@@ -3,6 +3,6 @@ import { createVNode } from "../../lib";
 
 export const Footer = () => (
   <footer className="bg-gray-200 p-4 text-center">
-    <p>&copy; 2024 항해플러스. All rights reserved.</p>
+    <p>&copy; ${new Date().getFullYear()} 항해플러스. All rights reserved.</p>
   </footer>
 );
